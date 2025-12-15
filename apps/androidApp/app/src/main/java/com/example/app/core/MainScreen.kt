03d6a3970cff1af5a7c73114cd8ddcc72d7fabd2
k-racubo <81630561+k-racubo@users.noreply.github.com>
2025-12-15 21:20:21 +0300

    Revert "refactor: optimized imports in MainScreen.kt"
    This reverts commit 20879832116558f009e1a10eb25df288b28fc8af.

--- apps/androidApp/app/src/main/java/com/example/app/core/MainScreen.kt
+++ apps/androidApp/app/src/main/java/com/example/app/core/MainScreen.kt
@@ -3,15 +3,25 @@ package com.example.app.core
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
+import org.intellij.lang.annotations.JdkConstants
 
 @Composable
 fun MainScreen() {
