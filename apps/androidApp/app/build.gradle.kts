111a9f61385d2b64b85d7cae4f851fbbdeb79bfe
k-racubo <81630561+k-racubo@users.noreply.github.com>
2025-12-15 21:20:21 +0300

    Revert "chore: fix build.gradle.kts incompatibility versions"
    This reverts commit 97a37a1c505e03542f3ae5d81660f7ad228c1028.

--- apps/androidApp/app/build.gradle.kts
+++ apps/androidApp/app/build.gradle.kts
@@ -6,13 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.app"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.app"
         minSdk = 24
-        //noinspection EditedTargetSdkVersion
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -40,22 +39,18 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
-    implementation("androidx.activity:activity-compose:1.12.0")
-
-    implementation(platform("androidx.compose:compose-bom:2025.11.01"))
-
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
     testImplementation("junit:junit:4.13.2")
-
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
