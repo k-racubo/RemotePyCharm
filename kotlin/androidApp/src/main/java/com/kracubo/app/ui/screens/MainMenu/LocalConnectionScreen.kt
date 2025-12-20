ae783593b4bbf0ddc1b8f8c1a3c0e91a3573d45f
kirill <kirill.tsyganov@internet.ru>
2025-12-20 18:39:14 +0300

    [mobile] removed UI elements in LocalConnectionScreen.kt

--- kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/MainMenu/LocalConnectionScreen.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/MainMenu/LocalConnectionScreen.kt
@@ -4,47 +4,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kracubo.app.core.viewmodel.mainmenu.LocalServerSearchScreenViewModel
-import com.kracubo.app.core.viewmodel.mainmenu.SearchState
 
 @Composable
 fun LocalConnectionScreen(
-    viewModel: LocalServerSearchScreenViewModel = viewModel(),
-    CancelServerSearch: () -> Unit)
-    {
-    val State = viewModel.searchState
-    when (State){
-        SearchState.SEARCHING -> {
+    exitToMainScreen: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
 
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    Text(text = "Поиск Сервера...")
-                    CircularProgressIndicator(
-                        modifier = Modifier.size(64.dp),
-                        strokeWidth = 4.dp
-                    )
-                    Button(onClick = { CancelServerSearch() }) {
-                        Text(text = "Выйти")
-                    }
-                }
-            }
         }
-        SearchState.FOUND -> {}
-        SearchState.ERROR -> {}
     }
 }
