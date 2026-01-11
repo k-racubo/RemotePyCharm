d0258842c689bfc08e0f7d181da8df826791dd7c
kirill <kirill.tsyganov@internet.ru>
2026-01-11 20:44:07 +0300

    [mobile] fix: changed code in NsdHelper.kt

--- kotlin/androidApp/src/main/java/com/kracubo/app/core/nsdManager/NsdHelper.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/core/nsdManager/NsdHelper.kt
@@ -5,14 +5,22 @@ import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import android.util.Log
 
+
 class NsdHelper(private val context: Context) {
+
     private val nsdManager: NsdManager = context.getSystemService(Context.NSD_SERVICE) as NsdManager
+
+    interface DiscoveryListener {
+        fun onServiceFound()
+        fun onError()
+    }
+
+    var listener: DiscoveryListener? = null
 
     fun discoverServices() {
         val serviceType = "_remotepycharm._tcp"
-        nsdManager.discoverServices(serviceType, NsdManager.PROTOCOL_DNS_SD,  object : NsdManager.DiscoveryListener {
-            override fun onDiscoveryStarted(regType: String?)
-            {
+        nsdManager.discoverServices(serviceType, NsdManager.PROTOCOL_DNS_SD, object : NsdManager.DiscoveryListener {
+            override fun onDiscoveryStarted(regType: String?) {
                 Log.i("NsdHelper", "Discovery started")
             }
 
@@ -22,18 +30,20 @@ class NsdHelper(private val context: Context) {
                     nsdManager.resolveService(it, object : NsdManager.ResolveListener {
                         override fun onResolveFailed(serviceInfo: NsdServiceInfo?, errorCode: Int) {
                             Log.e("NsdHelper", "Resolve failed: $errorCode")
+                            listener?.onError()
                         }
 
                         override fun onServiceResolved(serviceInfo: NsdServiceInfo?) {
                             Log.i("NsdHelper", "Service resolved: ${serviceInfo?.serviceName}")
                             val host = serviceInfo?.host?.hostAddress
                             val port = serviceInfo?.port
-                            Log.i("nsdHelper", "Host: ${host} Port:${port}")
-                            // Здесь можно использовать host и port для подключения к серверу
+                            Log.i("NsdHelper", "Host: $host Port: $port")
+                            listener?.onServiceFound()
                         }
                     })
                 }
             }
+
             override fun onServiceLost(serviceInfo: NsdServiceInfo?) {
                 Log.e("NsdHelper", "Service lost: ${serviceInfo?.serviceName}")
             }
@@ -44,10 +54,12 @@ class NsdHelper(private val context: Context) {
 
             override fun onStartDiscoveryFailed(serviceType: String?, errorCode: Int) {
                 Log.e("NsdHelper", "Start discovery failed: $errorCode")
+                listener?.onError()
             }
 
             override fun onStopDiscoveryFailed(serviceType: String?, errorCode: Int) {
                 Log.e("NsdHelper", "Stop discovery failed: $errorCode")
+                listener?.onError()
             }
         })
     }
