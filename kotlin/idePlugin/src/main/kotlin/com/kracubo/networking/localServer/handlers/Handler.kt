788781d5b59d50982d6a2a57803af73216496dcf
Nikitos <pzdankov@gmail.com>
2026-01-13 23:01:13 +0300

    [plugin] feat: enhance WebSocket message handler

--- kotlin/idePlugin/src/main/kotlin/com/kracubo/networking/localServer/handlers/Handler.kt
+++ kotlin/idePlugin/src/main/kotlin/com/kracubo/networking/localServer/handlers/Handler.kt
@@ -1,6 +1,26 @@
 package com.kracubo.networking.localServer.handlers
 
+import com.kracubo.core.project.ProjectManager
+import core.API
+import core.Response
+import kotlinx.serialization.json.Json
+import project.GetProjectsList
+import project.ProjectsListResponse
 
 class Handler {
+    val json = Json {
+        ignoreUnknownKeys = true
+        classDiscriminator = "type"
+    }
 
+    fun resolve(message: String) : Response {
+        return when (val apiMessage = json.decodeFromString(API.serializer(), message)) {
+            is GetProjectsList -> {
+                ProjectsListResponse(apiMessage.requestId, true,
+                    ProjectManager.getProjects(),
+                    ProjectManager.getCurrentProject())
+            }
+            else -> TODO()
+        }
+    }
 }
