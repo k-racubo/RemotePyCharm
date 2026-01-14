6c1929d555aa0747ea37f9893e1ef51bd6fe4da5
Nikitos <pzdankov@gmail.com>
2026-01-15 00:18:57 +0300

    [plugin] refactor: moved json object to api module for use everywhere

--- kotlin/idePlugin/src/main/kotlin/com/kracubo/networking/localServer/handlers/Handler.kt
+++ kotlin/idePlugin/src/main/kotlin/com/kracubo/networking/localServer/handlers/Handler.kt
@@ -1,20 +1,18 @@
 package com.kracubo.networking.localServer.handlers
 
+import com.kracubo.controlPanel.logger.Logger
+import com.kracubo.controlPanel.logger.SenderType
 import com.kracubo.core.project.ProjectManager
 import core.API
+import core.ApiJson
+import core.Command
 import core.Response
-import kotlinx.serialization.json.Json
 import project.GetProjectsList
 import project.ProjectsListResponse
 
 class Handler {
-    val json = Json {
-        ignoreUnknownKeys = true
-        classDiscriminator = "type"
-    }
-
     fun resolve(message: String) : Response {
-        return when (val apiMessage = json.decodeFromString(API.serializer(), message)) {
+        return when (val apiMessage = ApiJson.instance.decodeFromString<Command>(message)) {
             is GetProjectsList -> {
                 ProjectsListResponse(apiMessage.requestId, true,
                     ProjectManager.getProjects(),
