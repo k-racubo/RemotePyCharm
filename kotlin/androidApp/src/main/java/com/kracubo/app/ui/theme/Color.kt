d4b47384e3d3580224ef6c30c62a60d2eba7b4c4
kirill <kirill.tsyganov@internet.ru>
2026-01-18 21:34:06 +0300

    [mobile] fix: update colors in Theme

--- kotlin/androidApp/src/main/java/com/kracubo/app/ui/theme/Color.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.kracubo.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primary = Color(0xFF528BFF)
+val onPrimary = Color(0xFFFFFFFF)
+val primaryContainer = Color(0xFF2A3B60)
+val onPrimaryContainer = Color(0xFFB6CFFF)
+val secondary = Color(0xFF8FC8FF)
+val onSecondary = Color(0xFF172B45)
+val secondaryContainer = Color(0xFF1E3658)
+val onSecondaryContainer = Color(0xFFC9D9FF)
+val tertiary = Color(0xFFFF9C00)
+val onTertiary = Color(0xFF2E1500)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val background = Color(0xFF2B2B2B)
+val onBackground = Color(0xFFDADADA)
+
+val surface = Color(0xFF373A3C)
+val onSurface = Color(0xFFD0D0D0)
+
+val error = Color(0xFFEF5350)
+val onError = Color(0xFFFFFFFF)
