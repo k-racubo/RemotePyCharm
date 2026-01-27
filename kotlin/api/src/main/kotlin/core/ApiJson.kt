e75e8eb6f338ac6632cfb255592af619dffef9f2
Nikitos <pzdankov@gmail.com>
2026-01-28 02:50:20 +0300

    [api] feat: added new commands for serializer

--- kotlin/api/src/main/kotlin/core/ApiJson.kt
+++ kotlin/api/src/main/kotlin/core/ApiJson.kt
@@ -5,22 +5,31 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
-import project.GetProjectsList
-import project.ProjectsListResponse
+import project.list.GetProjectsList
+import project.list.ProjectsListResponse
+import project.open.OpenProjectCommand
+import project.open.ProjectFileTreeResponse
+import project.run.ResultOfRunResponse
+import project.run.RunCurrentConfigCommand
 
 object ApiJson {
     val instance = Json {
         ignoreUnknownKeys = true
         encodeDefaults = true
         classDiscriminator = "type"
+        isLenient = true
 
         serializersModule = SerializersModule {
             polymorphic(Command::class) {
                 subclass(GetProjectsList::class)
+                subclass(OpenProjectCommand::class)
+                subclass(RunCurrentConfigCommand::class)
             }
 
             polymorphic(Response::class) {
                 subclass(ProjectsListResponse::class)
+                subclass(ProjectFileTreeResponse::class)
+                subclass(ResultOfRunResponse::class)
             }
         }
     }
