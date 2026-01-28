9fa114bf617c754d9d5cc3719edcba14d10db3f3
Nikitos <pzdankov@gmail.com>
2026-01-28 03:06:15 +0300

    [plugin] fix: return "this" if string parsed not in JsonObject

--- kotlin/idePlugin/src/main/kotlin/com/kracubo/extensions/PrettyJsonString.kt
+++ kotlin/idePlugin/src/main/kotlin/com/kracubo/extensions/PrettyJsonString.kt
@@ -3,14 +3,17 @@ package com.kracubo.extensions
 import core.ApiJson
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 
 @OptIn(ExperimentalSerializationApi::class)
 fun String.prettyJson(): String {
-    return try {
+     try {
         val jsonElement = Json.parseToJsonElement(this)
 
+        if (jsonElement !is JsonObject) { return this }
+
         return ApiJson.pretty.encodeToString(jsonElement)
-    } catch (e: Exception) {
-        this // Return this string
+    } catch (_: Exception) {
+        return this // Return this string
     }
 }
