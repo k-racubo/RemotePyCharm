914933df573ee584b2058320b4271d4d98d0c74c
kirill <kirill.tsyganov@internet.ru>
2026-02-01 20:36:07 +0300

    [mobile] fix: changed enum class SearchState.kt

--- kotlin/androidApp/src/main/java/com/kracubo/app/core/ViewModel/MainMenu/SearchState.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/core/ViewModel/MainMenu/SearchState.kt
@@ -1,8 +1,11 @@
 package com.kracubo.app.core.viewmodel.mainmenu
 
 enum class  SearchState {
-    SEARCHING,
+    MDNS_SEARCHING,
+    CACHE_SEARCHING,
+    FULL_SEARCHING,
+    FULL_SEARCHING_HOLD,
+    QR_SEARCHING,
     FOUND,
     ERROR,
-    HOLD
 }
