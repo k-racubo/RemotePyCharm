ddf2abc5281271fb568c558e312e7650835ecf75
kirill <kirill.tsyganov@internet.ru>
2026-02-01 20:37:54 +0300

    [mobile] fix: sync LocalConnectionScreen.kt with new SearchState.kt

--- kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/MainMenu/LocalConnectionScreen.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/MainMenu/LocalConnectionScreen.kt
@@ -1,5 +1,6 @@
 package com.kracubo.app.ui.screens.mainmenu
 
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -27,6 +28,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,6 +38,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -49,17 +52,19 @@ import com.kracubo.app.ui.theme.TextColor
 
 @Composable
 fun LocalConnectionScreen(exitToMainScreen: () -> Unit) {
-
+    val context = LocalContext.current
     var searchPort by remember { mutableStateOf("") }
     val viewModel: LocalServerSearchScreenViewModel = viewModel()
     when (viewModel.searchState) {
         SearchState.ERROR -> {
+            exitToMainScreen()
+            Toast.makeText(context,"Connection failed", Toast.LENGTH_LONG).show()
+        }
+        SearchState.FOUND -> {
             Box(modifier = Modifier.fillMaxSize()) {
                 IconButton(
                     onClick = {
-                        if (viewModel.searchState == SearchState.HOLD) {
-                            exitToMainScreen()
-                        }
+                        exitToMainScreen()
                         viewModel.stopSearch()
                     },
                     modifier = Modifier
@@ -84,78 +89,14 @@ fun LocalConnectionScreen(exitToMainScreen: () -> Unit) {
                         text = "Подключение в локальной сети",
                         fontSize = 20.sp,
                     )
-                    Text(text = "Сервер не найден")
-                    Icon(
-                        imageVector = Icons.Filled.Warning,
-                        contentDescription = "Ошибка",
-                        modifier = Modifier.size(55.dp),
-                        tint = Color.Red
-                    )
+                    Text("local server was found")
                 }
             }
         }
-        SearchState.HOLD -> {
-            Box(modifier = Modifier.fillMaxSize()) {
-                IconButton(
-                    onClick = {
-                        if (viewModel.searchState == SearchState.HOLD) {
-                            exitToMainScreen()
-                        }
-                        viewModel.stopSearch()
-                    },
-                    modifier = Modifier
-                        .align(Alignment.TopStart)
-                        .padding(16.dp)
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "Назад",
-                        modifier = Modifier.size(30.dp),
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(top = 60.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    OutlinedTextField(
-                        value = searchPort,
-                        onValueChange = { searchPort = it },
-                        label = { Text("Server IP", color = LabelColor) },
-                        shape = RoundedCornerShape(12.dp),
-                        colors = OutlinedTextFieldDefaults.colors(
-                            focusedBorderColor = InputBorderColor,
-                            unfocusedBorderColor = InputBorderColor,
-                            focusedTextColor = TextColor,
-                            unfocusedTextColor = TextColor,
-                            cursorColor = TextColor,
-                            focusedContainerColor = Color.Transparent,
-                            unfocusedContainerColor = Color.Transparent
-                        ),
-                        modifier = Modifier.fillMaxWidth(0.75f),
-                    )
-                    Spacer(Modifier.height(20.dp))
-                    OutlinedButton(
-                        onClick = { viewModel.startSearch() },
-                        modifier = Modifier
-                            .fillMaxWidth(0.75f)
-                            .height(50.dp),
-                        shape = RoundedCornerShape(12.dp),
-                        border = BorderStroke(1.dp, ButtonBorderColor),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.Transparent,
-                            contentColor = ButtonTextColor
-                        )
-                    ) {
-                        Text(text = "Подключение")
-                    }
-                }
-            }
+        SearchState.QR_SEARCHING -> {
+
         }
-        SearchState.SEARCHING -> {
+        SearchState.FULL_SEARCHING -> {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
@@ -201,9 +142,7 @@ fun LocalConnectionScreen(exitToMainScreen: () -> Unit) {
                 }
                 IconButton(
                     onClick = {
-                        if (viewModel.searchState == SearchState.HOLD) {
-                            exitToMainScreen()
-                        }
+                        exitToMainScreen()
                         viewModel.stopSearch()
                     },
                     modifier = Modifier
@@ -224,13 +163,11 @@ fun LocalConnectionScreen(exitToMainScreen: () -> Unit) {
                 )
             }
         }
-        SearchState.FOUND -> {
+        SearchState.FULL_SEARCHING_HOLD ->{
             Box(modifier = Modifier.fillMaxSize()) {
                 IconButton(
                     onClick = {
-                        if (viewModel.searchState == SearchState.HOLD) {
-                            exitToMainScreen()
-                        }
+                        exitToMainScreen()
                         viewModel.stopSearch()
                     },
                     modifier = Modifier
@@ -251,13 +188,73 @@ fun LocalConnectionScreen(exitToMainScreen: () -> Unit) {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    Text(
-                        text = "Подключение в локальной сети",
-                        fontSize = 20.sp,
+                    OutlinedTextField(
+                        value = searchPort,
+                        onValueChange = { searchPort = it },
+                        label = { Text("Server IP", color = LabelColor) },
+                        shape = RoundedCornerShape(12.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = InputBorderColor,
+                            unfocusedBorderColor = InputBorderColor,
+                            focusedTextColor = TextColor,
+                            unfocusedTextColor = TextColor,
+                            cursorColor = TextColor,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent
+                        ),
+                        modifier = Modifier.fillMaxWidth(0.75f),
                     )
-                    Text("local server was found")
+                    Spacer(Modifier.height(20.dp))
+                    OutlinedButton(
+                        onClick = { viewModel.startSearch() },
+                        modifier = Modifier
+                            .fillMaxWidth(0.75f)
+                            .height(50.dp),
+                        shape = RoundedCornerShape(12.dp),
+                        border = BorderStroke(1.dp, ButtonBorderColor),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = ButtonTextColor
+                        )
+                    ) {
+                        Text(text = "Подключение")
+                    }
                 }
             }
+        }
+        SearchState.MDNS_SEARCHING -> {
+            viewModel.startSearch()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.5f)),
+                contentAlignment = Alignment.Center
+            ) {
+                IconButton(
+                    onClick = {
+                        exitToMainScreen()
+                        viewModel.stopSearch()
+                    },
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .padding(16.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Назад",
+                        modifier = Modifier.size(30.dp),
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+                CircularProgressIndicator(
+                    color = MaterialTheme.colorScheme.primary,
+                    strokeWidth = 4.dp,
+                    modifier = Modifier.size(60.dp)
+                )
+            }
+        }
+        SearchState.CACHE_SEARCHING -> {
+
         }
     }
 }
