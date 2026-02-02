30ef5281570ecbf94c083fbfc694333664ce4042
Nikitos <pzdankov@gmail.com>
2026-02-02 19:53:22 +0300

    [api] Now fileTree is JsonObject for creating project file tree

--- kotlin/api/src/main/kotlin/project/open/ProjectFileTreeResponse.kt
+++ kotlin/api/src/main/kotlin/project/open/ProjectFileTreeResponse.kt
@@ -3,11 +3,12 @@ package project.open
 import core.Response
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
 
 @Serializable
 @SerialName("PROJECT_FILE_TREE_RESPONSE")
 data class ProjectFileTreeResponse(
     override val requestId: String,
     override val success: Boolean,
-    val fileTree: Map<String, String>
+    val fileTree: JsonObject
 ) : Response()
