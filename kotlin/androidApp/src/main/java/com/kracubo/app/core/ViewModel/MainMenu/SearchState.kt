3cec075c70a86da4cd647b354aba95039ebef9e8
Nikitos <pzdankov@gmail.com>
2026-02-03 22:02:31 +0300

    [mobile] refactor: states associated with manual input (fallback) have been renamed

--- kotlin/androidApp/src/main/java/com/kracubo/app/core/ViewModel/MainMenu/SearchState.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/core/ViewModel/MainMenu/SearchState.kt
@@ -3,8 +3,8 @@ package com.kracubo.app.core.viewmodel.mainmenu
 enum class  SearchState {
     MDNS_SEARCHING,
     CACHE_SEARCHING,
-    FULL_SEARCHING,
-    FULL_SEARCHING_HOLD,
+    MANUAL_INPUT,
+    MANUAL_CONNECTING,
     QR_SEARCHING,
     FOUND,
     ERROR,
