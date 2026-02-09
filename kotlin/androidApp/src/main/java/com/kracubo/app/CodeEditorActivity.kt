850d0cc4fb29672d103abf894b61c70a95ae30e6
Friendly <kirill.tsyganov@internet.ru>
2026-02-09 20:39:18 +0300

    [mobile] feat: add 2 navigations in code editor activity

--- kotlin/androidApp/src/main/java/com/kracubo/app/CodeEditorActivity.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/CodeEditorActivity.kt
@@ -3,13 +3,33 @@ package com.kracubo.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.kracubo.app.ui.theme.AppTheme
+import androidx.navigation.compose.composable
+
 
 class CodeEditorActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         actionBar?.hide()
         setContent {
+            AppTheme() {
+                Surface(Modifier.fillMaxSize()) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "ListProjects") {
+                        composable("ListProjects"){
 
+                        }
+                        composable("CodeEditor") {
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
