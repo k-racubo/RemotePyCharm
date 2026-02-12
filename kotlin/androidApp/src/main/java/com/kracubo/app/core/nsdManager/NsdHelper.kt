9b0b0a25956f8c4a8c258ae790003fd6fc9780cf
Friendly <kirill.tsyganov@internet.ru>
2026-02-12 19:48:23 +0300

    [mobile] feat: cache searching has been completed

--- kotlin/androidApp/src/main/java/com/kracubo/app/core/nsdManager/NsdHelper.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/core/nsdManager/NsdHelper.kt
@@ -7,7 +7,7 @@ import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresExtension
 
-class NsdHelper(private val context: Context) {
+class NsdHelper(val context: Context) {
     private val nsdManager: NsdManager = context.getSystemService(Context.NSD_SERVICE) as NsdManager
 
     // flag for old resolveService()
