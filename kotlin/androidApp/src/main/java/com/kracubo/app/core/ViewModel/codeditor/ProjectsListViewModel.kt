918f8bcc451db1c6dadeee8664c4802a00e6d153
Nikitos <pzdankov@gmail.com>
2026-02-13 19:59:07 +0300

    [mobile] feat: base viewmodel for projects list screen

---
+++ kotlin/androidApp/src/main/java/com/kracubo/app/core/ViewModel/codeditor/ProjectsListViewModel.kt
@@ -0,0 +1,14 @@
+package com.kracubo.app.core.ViewModel.codeditor
+
+import androidx.lifecycle.ViewModel
+
+class ProjectsListViewModel : ViewModel() {
+
+    init {
+
+    }
+
+    override fun onCleared() {
+
+    }
+}
