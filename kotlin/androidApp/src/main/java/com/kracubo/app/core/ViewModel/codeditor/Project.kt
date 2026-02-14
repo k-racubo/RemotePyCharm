536590f83ac924b26d0f327cc32075f178ee810f
Friendly <kirill.tsyganov@internet.ru>
2026-02-14 20:53:35 +0300

    [mobile] refactor: add new data class for projects list

---
+++ kotlin/androidApp/src/main/java/com/kracubo/app/core/ViewModel/codeditor/Project.kt
@@ -0,0 +1,9 @@
+package com.kracubo.app.core.viewmodel.codeditor
+
+import androidx.compose.ui.graphics.Color
+
+data class Project(
+    val initials: String,
+    val name: String,
+    val color: Color
+)
