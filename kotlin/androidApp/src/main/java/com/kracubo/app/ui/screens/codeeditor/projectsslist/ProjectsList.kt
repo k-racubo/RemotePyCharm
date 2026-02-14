2f045f473631918de623ac1b6e5e2a93945f83a7
Nikitos <pzdankov@gmail.com>
2026-02-14 15:26:19 +0300

    [mobile] feat: setup viewmodel for screen

--- kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/codeeditor/projectsslist/ProjectsList.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/codeeditor/projectsslist/ProjectsList.kt
@@ -3,9 +3,13 @@ package com.kracubo.app.ui.screens.codeeditor.projectsslist
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kracubo.app.core.ViewModel.codeditor.ProjectsListViewModel
 
 @Composable
-public fun ProjectsList(toCodeEditor: () -> Unit){
+fun ProjectsList(toCodeEditor: () -> Unit) {
+    val viewmodel: ProjectsListViewModel = viewModel()
+
     Text("this is projects list")
     Button(onClick = {toCodeEditor()}) {
         Text("code editor")
