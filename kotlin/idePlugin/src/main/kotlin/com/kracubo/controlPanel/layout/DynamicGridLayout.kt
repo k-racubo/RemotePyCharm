9fc568cb2888df97bbc15b7c4590fd341ec31400
Nikitos <pzdankov@gmail.com>
2026-02-14 20:49:27 +0300

    [plugin] refactor: clean code and add stub dialog for remote server connection

--- kotlin/idePlugin/src/main/kotlin/com/kracubo/controlPanel/layout/DynamicGridLayout.kt
+++ kotlin/idePlugin/src/main/kotlin/com/kracubo/controlPanel/layout/DynamicGridLayout.kt
@@ -8,16 +8,9 @@ class DynamicGridLayout(rows: Int, cols: Int, hgap: Int, vgap: Int) : GridLayout
      * @see GridLayout
      */
 
-    // resize rows in GridLayout
     var dynamicRows: Int = rows
         set(value) {
             field = value
             this.rows = value
         }
-
-//    var dynamicCols: Int = cols
-//        set(value) {
-//            field = value
-//            this.columns = value
-//        } // maybe it will be needed for future features
 }
--- kotlin/idePlugin/src/main/kotlin/com/kracubo/controlPanel/logger/SenderType.kt
+++ kotlin/idePlugin/src/main/kotlin/com/kracubo/controlPanel/logger/SenderType.kt
@@ -3,5 +3,4 @@ package com.kracubo.controlPanel.logger
 enum class SenderType {
     LOGGER,
     LOCAL_SERVER,
-    REMOTE_SERVER,
 }
