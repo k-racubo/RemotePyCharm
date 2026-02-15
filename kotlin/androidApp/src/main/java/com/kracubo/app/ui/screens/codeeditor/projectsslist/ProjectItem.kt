b838965a88bdae5f2589c8e73b2302a071479322
Nikitos <pzdankov@gmail.com>
2026-02-15 13:14:50 +0300

    [mobile] feat: now project item have path under name

--- kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/codeeditor/projectsslist/ProjectItem.kt
+++ kotlin/androidApp/src/main/java/com/kracubo/app/ui/screens/codeeditor/projectsslist/ProjectItem.kt
@@ -3,9 +3,11 @@ package com.kracubo.app.ui.screens.codeeditor.projectsslist
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,14 +20,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kracubo.app.core.viewmodel.codeditor.Project
+import com.kracubo.app.core.viewmodels.codeditor.Project
 
 @Composable
-fun ProjectItem(project: Project, onClick: () -> Unit) {
+fun ProjectItem(project: Project, onClick: (Project) -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onClick() }
+            .clickable { onClick(project) }
             .padding(vertical = 6.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -42,9 +44,19 @@ fun ProjectItem(project: Project, onClick: () -> Unit) {
             )
         }
         Spacer(modifier = Modifier.width(12.dp))
-        Text(
-            text = project.name,
-            color = Color.White,
-            fontSize = 16.sp  )
+        Column {
+            Text(
+                text = project.name,
+                color = Color.White,
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(7.dp))
+            Text(
+                text = project.path,
+                color = Color.Gray,
+                fontSize = 9.sp
+            )
+        }
+
     }
 }
